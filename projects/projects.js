32284ec56c7a20eb6726be8c9d35adf90977e51c
Samuel Yuen <samuel.s.yuen@gmail.com>
2021-12-27 21:23:52 -0500

    removed old fetch

--- projects/projects.js
+++ projects/projects.js
@@ -48,23 +48,3 @@ window.fetch('https://us-central1-sisooyin.cloudfunctions.net/app/api/repos')
     // ADDS TABLE TO DOCUMENT
     document.querySelector('#main').innerHTML = tableHtml;
   })
-// const repoRequest = await fetch('http://localhost:5001/sisooyin/us-central1/app/api/repos')
-
-// const repoInformation = await repoRequest.json();
-// let counter = 1;
-// for (const repo of repoInformation.data) {
-//   if (repo.topics.includes('sisooyin') || repo.name === 'meet') {
-
-//     tableHtml += `<tr>
-//         <th scope="row">${counter}</th>
-//         <td><a href=${repo.html_url}>${repo.name}<a/></td>
-//         <td>${repo.description}</td>
-//         <td>${repo.language}</td>
-//         <td>${new Date(repo.updated_at).toLocaleDateString()}</td>
-//       </tr>`
-//     counter++;
-//   }
-// }
-
-
-
