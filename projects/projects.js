4f0852bf78df15dfd33effaf0686b29f52042a26
Samuel Yuen <samuel.s.yuen@gmail.com>
2021-12-27 21:23:23 -0500

    implemented mobile safe fetch

--- projects/projects.js
+++ projects/projects.js
@@ -1,4 +1,5 @@
 import '../style.css'
+import 'whatwg-fetch';
 
 let tableHtml = `<table class="table table-striped table-responsive">
 <thead>
@@ -14,34 +15,56 @@ let tableHtml = `<table class="table table-striped table-responsive">
 `
 
 // GETTING PERSONAL REPOS
-const repoRequest = await fetch('https://us-central1-sisooyin.cloudfunctions.net/app/api/repos')
+window.fetch('https://us-central1-sisooyin.cloudfunctions.net/app/api/repos')
+  .then(resp => resp.json())
+  .then(resp => {
+    let counter = 1;
+    for (const repo of resp.data) {
+      console.log(repo)
+
+      if (repo.topics.includes('sisooyin') || repo.name === 'meet') {
+
+        tableHtml += `<tr>
+            <th scope="row">${counter}</th>
+            <td><a href=${repo.html_url}>${repo.name}<a/></td>
+            <td>${repo.description}</td>
+            <td>${repo.language}</td>
+            <td>${new Date(repo.updated_at).toLocaleDateString()}</td>
+          </tr>`
+        counter++;
+      }
+    }
+  }).then(() => {
+    tableHtml += `</tbody>
+    </table>`
+    console.log(tableHtml)
+
+    // REMOVES CONTENT CENTERING IN MIDDLE OF PAGE
+    document.querySelector('#main').classList.remove("position-absolute")
+    document.querySelector('#main').classList.remove("top-50")
+    document.querySelector('#main').classList.remove("start-50")
+    document.querySelector('#main').classList.remove("translate-middle")
+
+    // ADDS TABLE TO DOCUMENT
+    document.querySelector('#main').innerHTML = tableHtml;
+  })
 // const repoRequest = await fetch('http://localhost:5001/sisooyin/us-central1/app/api/repos')
 
-const repoInformation = await repoRequest.json();
-let counter = 1;
-for (const repo of repoInformation.data) {
-  if (repo.topics.includes('sisooyin') || repo.name === 'meet') {
+// const repoInformation = await repoRequest.json();
+// let counter = 1;
+// for (const repo of repoInformation.data) {
+//   if (repo.topics.includes('sisooyin') || repo.name === 'meet') {
 
-    tableHtml += `<tr>
-        <th scope="row">${counter}</th>
-        <td><a href=${repo.html_url}>${repo.name}<a/></td>
-        <td>${repo.description}</td>
-        <td>${repo.language}</td>
-        <td>${new Date(repo.updated_at).toLocaleDateString()}</td>
-      </tr>`
-    counter++;
-  }
-}
+//     tableHtml += `<tr>
+//         <th scope="row">${counter}</th>
+//         <td><a href=${repo.html_url}>${repo.name}<a/></td>
+//         <td>${repo.description}</td>
+//         <td>${repo.language}</td>
+//         <td>${new Date(repo.updated_at).toLocaleDateString()}</td>
+//       </tr>`
+//     counter++;
+//   }
+// }
 
 
-tableHtml += `</tbody>
-</table>`
 
-// REMOVES CONTENT CENTERING IN MIDDLE OF PAGE
-document.querySelector('#main').classList.remove("position-absolute")
-document.querySelector('#main').classList.remove("top-50")
-document.querySelector('#main').classList.remove("start-50")
-document.querySelector('#main').classList.remove("translate-middle")
-
-// ADDS TABLE TO DOCUMENT
-document.querySelector('#main').innerHTML = tableHtml;
